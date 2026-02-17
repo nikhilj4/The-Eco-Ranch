378a4a84795409986a3987d2c213636e46729e40
Nikhil J <nikhiljram4@gmail.com>
2026-02-17 10:54:39 +0530

    Add FocusRail component and section

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { HeroHeader } from './components/HeroHeader';
 import { HeroSection } from './components/HeroSection';
+import { FocusRailSection } from './components/FocusRailSection';
 import { SignatureExperiences } from './components/SignatureExperiences';
 import { PremiumAddOns } from './components/PremiumAddOns';
 import { SEOContent } from './components/SEOContent';
@@ -17,6 +18,7 @@ export default function App() {
     <div className="min-h-screen bg-stone-50 selection:bg-[#F2A65A]/30">
       <HeroHeader />
       <HeroSection />
+      <FocusRailSection />
       <SignatureExperiences />
       <PremiumAddOns />
       <Gallery />
